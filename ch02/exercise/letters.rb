481309601b0577ebca990a44729b2b029faa102b
Georgiy Slobodenyuk <youanden@gmail.com>
2012-09-03 08:23:21 -0400

    Completed following Chapter 2

---
+++ ch02/exercise/letters.rb
@@ -0,0 +1,24 @@
+'Hello.'
+'5 is my favorite number... what is yours?'
+'Snoopy says #%^?&*@! when he stubs his toe.'
+'     '
+''
+puts 'Hello, world!'
+puts ''
+puts 'Good-bye'
+puts 'I like ' + 'apple pie.'
+puts 'I like' + ' apple pie.'
+puts 'blink ' * 4
+puts  12  +  12
+puts '12' + '12'
+puts '12  +  12'
+puts  2  *  5
+puts '2' *  5
+puts '2  *  5'
+# puts 'Betty' + 12
+# puts 'Fred' * 'John'
+puts 'You\'re swell!'
+puts 'backslash at the end of a string:  \\'
+puts 'up\\down'
+puts 'up\down'
+
