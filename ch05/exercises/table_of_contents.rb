672792e5e3ca16d4f8afef398fe43051fd7da02a
Georgiy Slobodenyuk <youanden@gmail.com>
2012-09-03 14:21:08 -0400

    More advanced version of the table of contents program

--- ch05/exercises/table_of_contents.rb
+++ ch05/exercises/table_of_contents.rb
@@ -1,10 +1,12 @@
 maxwidth = 60
-ch1 = [1,"Numbers",1]
-ch2 = [2,"Letters",72]
-ch3 = [3,"Variables",118]
+chapters = [
+  ["Numbers",1],
+  ["Letters",72],
+  ["Variables",118]
+]
 
 puts "Table of Contents\n"
-puts "Chapter #{ch1[0]}:".ljust(maxwidth/3) + "#{ch1[1]}".ljust(maxwidth/3) + "page #{ch1[2]}".rjust(maxwidth/3) 
-puts "Chapter #{ch2[0]}:".ljust(maxwidth/3) + "#{ch2[1]}".ljust(maxwidth/3) + "page #{ch2[2]}".rjust(maxwidth/3) 
-puts "Chapter #{ch3[0]}:".ljust(maxwidth/3) + "#{ch3[1]}".ljust(maxwidth/3) + "page #{ch3[2]}".rjust(maxwidth/3) 
+chapters.each_with_index do |chapter, index|
+  puts "Chapter #{index+1}".ljust(maxwidth/3) + "#{chapter[0]}".ljust(maxwidth/3) + "page #{chapter[1]}".rjust(maxwidth/3)
+end
 
