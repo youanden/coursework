d38cca19409e3ba406c17f7680bc9dfaf9ea3ca6
Georgiy Slobodenyuk <youanden@gmail.com>
2012-09-03 08:36:17 -0400

    Completed following Chapter 3

---
+++ ch03/exercise/vars.rb
@@ -0,0 +1,26 @@
+myString = '...you can say that again...'
+puts myString
+puts myString
+name = 'Patricia Rosanna Jessica Mildred Oppenheimer'
+puts 'My name is ' + name + '.'
+puts 'Wow!  ' + name + ' is a really long name!'
+composer = 'Mozart'
+puts composer + ' was "da bomb", in his day.'
+
+composer = 'Beethoven'
+puts 'But I prefer ' + composer + ', personally.'
+var = 'just another ' + 'string'
+puts var
+
+var = 5 * (1+2)
+puts var
+var1 = 8
+var2 = var1
+puts var1
+puts var2
+
+puts ''
+
+var1 = 'eight'
+puts var1
+puts var2
