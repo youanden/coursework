3688666fe8afba247cf50c651950a000ceb83563
Georgiy Slobodenyuk <youanden@gmail.com>
2012-09-03 09:28:59 -0400

    Completed Reflecting on Exercise 1

---
+++ reflections/exercise_01.rb
@@ -0,0 +1,15 @@
+# 1. Puts add a newline at the end of a file. Print does not.
+
+# 2. Large Number Literals: 
+# One Million
+1_0_0_0_0_0_0
+
+# 3a. to_s converts the object to a string
+# 3b. to_i converts the object to an integer
+# 3c. puts takes a string as an argument or if given a non-string object will call the to_s method on that object as a return value
+# 3d. gets pulls a string from input
+# 3e. chomp trims the carriage return characters like \n and \r (or both).
+
+# 4. Interpolation
+
+# E. In ruby, everything is an object, even + (a method), and so values after a + act as parameters that can be worked on or refined in a method to clean output, or prepare the parameter for addition
