899c8f9f028a4e4f74f4fb08d34cd487a9ab6bb7
Georgiy Slobodenyuk <youanden@gmail.com>
2012-09-03 08:10:41 -0400

    Completed Exercise 1

--- calc.rb
+++ calc.rb
@@ -1,4 +1,29 @@
-puts 1.0 + 2.0
-puts 2.0 * 3.0
-puts 5.0 - 8.0
-puts 9.0 / 2.0
+# Initial Program Reference
+# puts 1.0 + 2.0
+# puts 2.0 * 3.0
+# puts 5.0 - 8.0
+# puts 9.0 / 2.0
+# End Initial Program
+
+# Hours in a year:
+# Hours per day * days in a year
+puts 24*365
+# Minutes in a decade:
+# Minutes in an hour * hours in a
+# day * days in a year * 10 years
+# Not accounting for leap years
+puts 60*24*365*10
+# Seconds old am I:
+# Seconds in a minute * 
+# Minutes in an hour *
+# Hours in a day *
+# Days in a year *
+# Years I am alive
+puts 60*60*24*365*20
+# Chocolates hoping / life
+puts 1*365*80
+
+# Tough Guy:
+# If I am x seconds old, how old am I
+# in years?
+puts 630720000/(60*60*24*365) 
