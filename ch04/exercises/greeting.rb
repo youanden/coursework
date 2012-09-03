a4f5acf33aa1e11a12dfa47074f61f3f3dab2220
Georgiy Slobodenyuk <youanden@gmail.com>
2012-09-03 08:55:48 -0400

    Added greeting program

---
+++ ch04/exercises/greeting.rb
@@ -0,0 +1,9 @@
+# Try 1: Greeting
+puts 'First?'
+fname = gets.chomp
+puts 'Middle Name?'
+mname = gets.chomp
+puts 'Last Name?'
+lname = gets.chomp
+puts ''
+puts 'Yo, ' + fname + ' ' + mname + ' ' + lname
